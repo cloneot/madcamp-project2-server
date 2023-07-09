16ea3ab59970838ccd24c7cd82244c139a1a64a3
mingyu0304 <mingyu030304@gmail.com>
2023-07-09 16:24:13 +0000

    feat(scheme): change users table

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,25 @@
 const router = require('express').Router();
+const connection = require('../db');
 
 router.get('/test', (req, res) => {
 	console.log(req);
 	return res.json([{id: 17, username: "junseo"}]);
 });
 
-// router.route('/users/:id').get((req, res) => {
-
-// });
+router.get('/users/:username', (req, res) => {
+	let username = req.params.username;
+	console.log(username);
+	// console.log(req);
+	connection.query('SELECT * FROM users WHERE username = ?', [username], function(error, results, fields) {
+		console.log(results);
+		if(error)	throw error;
+		if(results.length > 0) {
+			return res.json(results[0]);
+		}
+		else {
+			return res.status(200).json();
+		}
+	});
+});
 
 module.exports = router;
